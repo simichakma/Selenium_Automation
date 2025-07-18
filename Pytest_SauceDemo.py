4820a39ee89152594729e3fc78a7d6b5f28ba75c
simi chakma <simichakma139@gmail.com>
2025-07-19 00:42:12 +0600

    Update Pytest_SauceDemo.py

--- Pytest_SauceDemo.py
+++ Pytest_SauceDemo.py
@@ -1,11 +1,12 @@
-import pytest
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 import time
+import pytest
 
-@pytest.fixture
+
+@pytest.fixture(scope="module")
 def driver():
     options = Options()
     options.add_argument("--incognito")
@@ -20,92 +21,94 @@ def driver():
     driver.get("https://www.saucedemo.com/")
     time.sleep(2)
     yield driver
-    driver.quit()
+    return driver
 
-#login_saucedemo
-def login_with_attribute(driver, username, password):
+# Login the page
+@pytest.mark.parametrize("username, password", [("standard_user", "secret_sauce")])
+@pytest.mark.order(1)
+@pytest.mark.dependency(name="login")
+def test_login(driver, username, password):
     driver.find_element(By.ID, "user-name").send_keys(username)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.ID, "login-button").click()
+    assert "inventory" in driver.current_url
 
-# Login using short XPath
-def login_with_xpath(driver, username, password):
-    driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys(username)
-    driver.find_element(By.XPATH, "//input[@id='password']").send_keys(password)
-    driver.find_element(By.XPATH, "//input[@id='login-button']").click()
 
-#sorting_products: A-Z, High-Low, Low-High
-def sort_products(driver, sort_type):
-    dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
-    dropdown.select_by_value(sort_type)
+# Sorting to ["a-z","low-high","high-low"]
+@pytest.mark.parametrize("sort_type", ["az", "lohi", "hilo"])
+@pytest.mark.order(2)
+@pytest.mark.dependency(depends=["login"])
+def test_sort_products(driver, sort_type):
+    sort_dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
+    sort_dropdown.select_by_value(sort_type)  # "az","lohi", "hilo"
     time.sleep(2)
 
-def add_to_cart(driver):
+#add to cart
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["login"])
+def test_add_to_cart(driver):
     time.sleep(2)
     driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
 
-#Add_to_specific_product
-def add_specific_product_to_cart(driver, product_name):
+
+# Add to specific product
+@pytest.mark.parametrize("product_name", [
+     "Sauce Labs Bolt T-Shirt"])
+@pytest.mark.order(4)
+@pytest.mark.dependency(depends=["login"])
+def test_add_specific_product_to_cart(driver, product_name ):
     xpath = f"//div[text()='{product_name}']/ancestor::div[@class='inventory_item']//button"
     driver.find_element(By.XPATH, xpath).click()
     print(f"Added: {product_name}")
     time.sleep(2)
 
-#Add_to_multiple_products
-def add_multiple_products(driver):
-    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-    driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
-    driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-    time.sleep(2)
+#Add_to_multiple_items
+@pytest.mark.parametrize("product_ID", [
+    "add-to-cart-sauce-labs-backpack",
+    "add-to-cart-sauce-labs-bolt-t-shirt"
+])
+@pytest.mark.order(5)
+@pytest.mark.dependency(depends=["login"])
+def test_add_multiple_products(driver, product_ID):
+    driver.find_element(By.ID, product_ID).click()
+    print(f"Added: {product_ID}")
+    time.sleep(1)
 
-def go_to_cart(driver):
+# Go to cart page
+@pytest.mark.order(6)
+@pytest.mark.dependency(depends=["login"])
+def test_go_to_cart(driver):
     driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
     time.sleep(2)
 
-#Remove_to_cart
-def remove_products_from_cart(driver):
-    driver.find_element(By.ID, "remove-sauce-labs-bike-light").click()
-    driver.find_element(By.ID, "remove-sauce-labs-bolt-t-shirt").click()
+#Remove the one item
+@pytest.mark.parametrize("remove_button_ID", [
+    "remove-sauce-labs-bolt-t-shirt"
+])
+@pytest.mark.order(7)
+@pytest.mark.dependency(depends=["login"])
+def test_remove_items_from_cart(driver, remove_button_ID):
+    driver.find_element(By.ID, remove_button_ID).click()
+    print(f"Removed: {remove_button_ID}")
     time.sleep(2)
 
-def checkout(driver):
+# Click checkout button
+@pytest.mark.order(8)
+@pytest.mark.dependency(depends=["login"])
+def test_checkout(driver):
     driver.find_element(By.ID, "checkout").click()
-    time.sleep(2)
-
-def customer_info(driver, first_name, last_name, postal_code):
-    driver.find_element(By.ID, "first-name").send_keys(first_name)
-    driver.find_element(By.ID, "last-name").send_keys(last_name)
-    driver.find_element(By.ID, "postal-code").send_keys(postal_code)
+    driver.find_element(By.ID, "first-name").send_keys("Simi")
+    driver.find_element(By.ID, "last-name").send_keys("Chakma")
+    driver.find_element(By.ID, "postal-code").send_keys("124simichakma")
     driver.find_element(By.ID, "continue").click()
-    time.sleep(2)
+    assert "checkout-step-two" in driver.current_url
 
-def payment_cart(driver):
-    assert driver.find_element(By.CLASS_NAME, "title").text()
-    print("Payment_cart page confirmed")
-    time.sleep(2)
-
-#Logout
-def logout(driver):
+# Logout
+@pytest.mark.order(9)
+@pytest.mark.dependency(depends=["login"])
+def test_logout(driver):
     driver.find_element(By.ID, "react-burger-menu-btn").click()
-    time.sleep(2)
+    time.sleep(1)
     driver.find_element(By.ID, "logout_sidebar_link").click()
-    time.sleep(2)
-
-
- #Pytest Dependency Test Flow:
-
-@pytest.mark.dependency()
-def test_full_saucedemo_flow(driver):
-    login_with_attribute(driver, "standard_user", "secret_sauce")
-    #sort_products(driver, "az")
-    #sort_products(driver, "lohi")
-    #sort_products(driver, "hilo")
-    add_to_cart(driver)
-    #add_specific_product_to_cart(driver, "Sauce Labs Bolt T-Shirt")
-    #add_multiple_products(driver)
-    go_to_cart(driver)
-    #remove_products_from_cart(driver)
-    checkout(driver)
-    customer_info(driver, "simi", "chakma", "124simichakma")
-    payment_cart(driver)
-    #logout(driver)
+    time.sleep(1)
+    assert "saucedemo" in driver.current_url
